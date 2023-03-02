5d2aa416e0fc3730f5192f589dafee6431c3e099
saywako <124358844+saywako@users.noreply.github.com>
2023-03-02 09:22:02 -0300

    Add files via upload

--- Aula 15/app.js
+++ Aula 15/app.js
@@ -1,25 +1,48 @@
+// 1. Change color of car and addToCart button color when a color is selected
+// - Selecting Elements
 const redColor = document.querySelector(".red");
 const grayColor = document.getElementsByClassName("gray");
 const blackColor = document.querySelector(".black");
 const cartButton = document.getElementById("button");
 const itemTag = document.getElementsByTagName("h3")[0];
 const imageCard = document.querySelector(".product-image");
-const feedbackBth = document.querySelector("feedback");
+const feedbackBtn = document.querySelector(".feedback");
 
-redColor.addEventListener("click", function(){
-    cartButton.style.backgroundColor = "red";
-    itemTag.style.backgroundColor = "red";
-    imageCard.style.backgroundImage = 'url'("./img/red-benz.webp");
+// Modifying Elements
+// - Add Event Listeners
+// - Red Color
+redColor.addEventListener("click", function () {
+  cartButton.style.backgroundColor = "red";
+  itemTag.style.backgroundColor = "red";
+  imageCard.style.backgroundImage = 'url("./img/MicrosoftTeams-image\ \(1\).png")';
 });
 
-grayColor[0].addEventListener("click", function(){
-    cartButton.style.backgroundColor = "gray";
-    itemTag.style.backgroundColor = "gray";
-    imageCard.style.backgroundImage = 'url'("./img/gray-benz.webp");
+// - Gray Color
+grayColor[0].addEventListener("click", function () {
+  cartButton.style.backgroundColor = "gray";
+  itemTag.style.backgroundColor = "gray";
+  imageCard.style.backgroundImage = 'url("./img/MicrosoftTeams-image\ \(2\).png")';
 });
 
-blackColor.addEventListener("click", function(){
-    cartButton.style.backgroundColor = "black";
-    itemTag.style.backgroundColor = "black";
-    imageCard.style.backgroundImage = 'url'("./img/black-benz.webp");
+// - Black Color
+blackColor.addEventListener("click", function () {
+  cartButton.style.backgroundColor = "black";
+  itemTag.style.backgroundColor = "black";
+  imageCard.style.backgroundImage = 'url("./img/MicrosoftTeams-image.png")';
 });
+
+// Button Click Implementation
+// - Cart Button
+const cart = () => {
+  cartButton.style.display = "none";
+  feedbackBtn.style.display = "block";
+};
+cartButton.addEventListener("click", cart);
+
+// - Feedback Button
+const feedback = () => {
+  cartButton.style.display = "block";
+  feedbackBtn.style.display = "none";
+};
+feedbackBtn.addEventListener("click", feedback);
+
